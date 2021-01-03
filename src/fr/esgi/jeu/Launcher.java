18e05d4c32979b0ff183ca00d6d727325a1b567f
CatLeChat <ae48YB(+>
2021-01-03 10:51:12 +0100

    fixed warnings

--- src/fr/esgi/jeu/Launcher.java
+++ src/fr/esgi/jeu/Launcher.java
@@ -1,7 +1,5 @@
 package fr.esgi.jeu;
 
-
-import java.util.Hashtable;
 import java.util.Random;
 import java.util.Scanner;
 
@@ -15,7 +13,7 @@ public class Launcher {
 	private static final int MIN_PLAYERS = 2;
 	private static final int MAX_PLAYERS = 8;
 
-	public static void main(String args[]) {
+	public static void main(String[] args) {
 		try ( Scanner scanner = new Scanner( System.in ) ) {
 			while (true){
 				System.out.println("_________Welcome to the game !_________");
@@ -23,67 +21,61 @@ public class Launcher {
 				System.out.println("_________1. New Game___________________");
 				System.out.println("_________2. Continue___________________");
 				System.out.println("_________3. Exit_______________________");
-				switch (scanner.nextInt()){
-					case 1: Random rand = new Random();
+				switch (scanner.nextInt()) {
+					case 1 -> {
+						Random rand = new Random();
 						System.out.println("Game is starting...");
-						int playerCount = 0;
-						int boardName = 0;
-						int mapSize = 0;
-						//HOW MUCH PLAYERS
+						int playerCount;
+						int boardName;
+						int mapSize;
 						do {
 							System.out.println("Please enter the number of players:");
 							playerCount = scanner.nextInt();
-							if (playerCount>MAX_PLAYERS){
-								System.out.println("Too much players, maximum is "+MAX_PLAYERS);
-							}else if(playerCount<MIN_PLAYERS){
-								System.out.println("Not enouth players, minimum is "+MIN_PLAYERS);
+							if (playerCount > MAX_PLAYERS) {
+								System.out.println("Too much players, maximum is " + MAX_PLAYERS);
+							} else if (playerCount < MIN_PLAYERS) {
+								System.out.println("Not enouth players, minimum is " + MIN_PLAYERS);
 							}
-						}while(playerCount>MAX_PLAYERS || playerCount < MIN_PLAYERS);
-						//PLAYERS NAMES
+						} while (playerCount > MAX_PLAYERS || playerCount < MIN_PLAYERS);
 						Player[] players = new Player[playerCount];
-						for(int i = 0; i<playerCount;i++) {
-							String name = "";
-							System.out.println("Please enter the name of player n°"+(i+1)+":");
+						for (int i = 0; i < playerCount; i++) {
+							String name;
+							System.out.println("Please enter the name of player n°" + (i + 1) + ":");
 							name = scanner.next();
 							players[i] = new Player(name);
 						}
-						//MAP SELECTION
-						//MAP NAME
 						do {
 							System.out.println("Please select the map:");
 							for (int i = 0; i < BOARD_NAMES.length; i++) {
-								System.out.print((i+1)+":"+BOARD_NAMES[i]+" ");
+								System.out.print((i + 1) + ":" + BOARD_NAMES[i] + " ");
 							}
 							System.out.print("\n");
 							boardName = scanner.nextInt();
-							if (boardName<=0 || boardName>BOARD_NAMES.length) {
+							if (boardName <= 0 || boardName > BOARD_NAMES.length) {
 								System.out.println("No such map");
 							}
-						}while(boardName<=0 || boardName>BOARD_NAMES.length);
-						boardName = boardName -1;
-						//MAP SIZE
+						} while (boardName <= 0 || boardName > BOARD_NAMES.length);
+						boardName = boardName - 1;
 						do {
-							System.out.println("Please select the map size (between "+MIN_MAP_SIZE+" and "+MAX_MAP_SIZE+" ):");
+							System.out.println("Please select the map size (between " + MIN_MAP_SIZE + " and " + MAX_MAP_SIZE + " ):");
 							mapSize = scanner.nextInt();
-							if (mapSize<MIN_MAP_SIZE){
+							if (mapSize < MIN_MAP_SIZE) {
 								System.out.println("Too short");
-							}else if(mapSize>MAX_MAP_SIZE){
+							} else if (mapSize > MAX_MAP_SIZE) {
 								System.out.println("Too big");
 							}
-						}while(mapSize<MIN_MAP_SIZE || mapSize>MAX_MAP_SIZE);
-						//BOARD GENERATION
-						Board board = new Board(BOARD_NAMES[boardName],mapSize);
+						} while (mapSize < MIN_MAP_SIZE || mapSize > MAX_MAP_SIZE);
+						Board board = new Board(BOARD_NAMES[boardName], mapSize);
 						board.generateSquares();
-						//GAME INIT
-						Game game1 = new Game(rand.nextInt(255)+1, board, players);
-						//GAME START
+						Game game1 = new Game(rand.nextInt(255) + 1, board, players);
 						game1.start();
-						break;
-					case 2: System.out.println("not implemented yet");
-						break;
-					case 3: System.out.println("Exiting the game.. See ya next time !");
+					}
+					case 2 -> System.out.println("Here you'll be able to load a game you saved");
+					case 3 -> {
+						System.out.println("Exiting the game.. See ya next time !");
 						return;
-					default: System.out.println("not implemented yet");
+					}
+					default -> System.out.println("not implemented yet");
 				}
 			}
 		}
